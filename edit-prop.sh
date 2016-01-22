35764619f86aeb92f43ef094af17cf656ebb521e
Ethan Nelson-Moore <parrotgeek1@gmail.com>
2016-01-21 19:09:15 -0600

    initial commit

---
+++ edit-prop.sh
@@ -0,0 +1,9 @@
+#!/sbin/sh
+
+busybox grep -F -v -q 'persist.debug.wfd.enable=1' /system/build.prop && echo 'persist.debug.wfd.enable=1' >> /system/build.prop
+
+busybox grep -F -v -q 'ro.config.wallpaper=' /system/build.prop && echo 'ro.config.wallpaper=/system/media/wallpaper.jpg' >> /system/build.prop
+
+busybox sed -i 's/ro.setupwizard.wifi_required=true/ro.setupwizard.wifi_required=false/g' /system/build.prop
+
+busybox sed -i 's/id=LMY47V/id=LMY47V_ParrotMod_v1/g' /system/build.prop
