02776652dbe157acbcbeccaa750f9f584d97f849
Ethan <parrotgeek1@gmail.com>
2016-01-22 11:53:08 -0600

    Update magic.sh

--- system/etc/parrotmod/magic.sh
+++ system/etc/parrotmod/magic.sh
@@ -53,7 +53,10 @@ $bb renice -7 $($bb pidof hd-audio0) #avoid underruns but can't be faster than m
 $bb renice -10 $($bb pidof sdcard)
 $bb renice -15 $($bb pidof lmkd) # stop hard freezes from low memory killer being CPU starved
 settings put global wifi_networks_available_notification_on 0
+settings put global wifi_display_certification_on 1
+settings put global verifier_verify_adb_installs 0
 settings put global sys_storage_full_threshold_bytes 8388608
 settings put global sys_storage_threshold_percentage 2
 settings put global sys_storage_threshold_max_bytes 104857600
+settings put global wifi_allow_scan_with_traffic 1
 $bb date > /data/local/tmp/parrotmod.txt
