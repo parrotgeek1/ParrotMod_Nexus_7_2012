60c940c14762f913377bca7588e197bef945afa1
Ethan <parrotgeek1@gmail.com>
2016-01-22 12:33:36 -0600

    can replace old mod version

--- edit-prop.sh
+++ edit-prop.sh
@@ -2,7 +2,7 @@
 busybox grep -F -v -q 'persist.debug.wfd.enable=1' /system/build.prop && echo 'persist.debug.wfd.enable=1' >> /system/build.prop
 busybox grep -F -v -q 'ro.config.wallpaper=' /system/build.prop && echo 'ro.config.wallpaper=/system/media/wallpaper.jpg' >> /system/build.prop
 busybox sed -i 's/ro.setupwizard.wifi_required=true/ro.setupwizard.wifi_required=false/g' /system/build.prop
-busybox sed -i 's/id=LMY47V$/id=LMY47V_ParrotMod_v1/g' /system/build.prop
+busybox sed -i 's/id=LMY47V.*$/id=LMY47V_ParrotMod_v1/g' /system/build.prop
 busybox sed -i 's/=user$/=userdebug/g' /system/build.prop # for hdcp etc settings
 busybox grep -F -v -q 'persist.sys.hdcp_checking=' /system/build.prop && echo 'persist.sys.hdcp_checking=never' >> /system/build.prop
 busybox grep -F -v -q 'ro.isparrotmod=1' /system/build.prop && echo 'ro.isparrotmod=1' >> /system/build.prop
