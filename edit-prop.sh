aece0399bb063b4d54f4c10cfd7309dd8677f7d9
Ethan Nelson-Moore <parrotgeek1@gmail.com>
2016-01-25 17:32:42 -0600

    keep previous

--- edit-prop.sh
+++ edit-prop.sh
@@ -2,5 +2,5 @@
 busybox grep -F -v -q 'persist.debug.wfd.enable=1' /system/build.prop && echo 'persist.debug.wfd.enable=1' >> /system/build.prop
 busybox grep -F -v -q 'ro.config.wallpaper=' /system/build.prop && echo 'ro.config.wallpaper=/system/media/wallpaper.jpg' >> /system/build.prop
 busybox grep -F -v -q 'ro.setupwizard.wifi_required=' /system/build.prop && echo 'ro.setupwizard.wifi_required=false' >> /system/build.prop
-busybox sed -i 's/id=LMY47V.*$/id=LMY47V_ParrotMod_2016-01-25_17:27:31/g' /system/build.prop
+busybox sed -i 's/id=LMY47V.*$/id=LMY47V_ParrotMod_2016-01-25_17:32:33/g' /system/build.prop
 busybox grep -F -v -q 'persist.sys.wfd.noimr=1' /system/build.prop && echo 'persist.sys.wfd.noimr=1' >> /system/build.prop
