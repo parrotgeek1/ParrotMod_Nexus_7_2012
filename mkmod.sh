b902efefcad53cae1d652f93de5afb42e1aaf56f
Ethan Nelson-Moore <parrotgeek1@gmail.com>
2016-02-08 11:56:19 -0600

    directory structure change

--- mkmod.sh
+++ mkmod.sh
@@ -1,5 +1,4 @@
-cd "$(dirname "$0")"
+cd "$(dirname "$0")/zip"
 rm -rf ../ParrotMod_Grouper_Stable.zip
 find . -name '.DS_Store' -delete
-sed -i '' "s@LMY47V_ParrotMod_.*/g@LMY47V_ParrotMod_$(date +%F_%T)/g@g" edit-prop.sh
 zip -9 -r ../ParrotMod_Grouper_Stable.zip * 
--- upmod.sh
+++ upmod.sh
@@ -1,4 +1,3 @@
 cd "$(dirname "$0")"
-rm -f ~/Google\ Drive/Website\ Downloads/Android/ParrotMod_Grouper_Previous.zip
-mv ~/Google\ Drive/Website\ Downloads/Android/ParrotMod_Grouper_Stable.zip ../ParrotMod_Grouper_Previous.zip 
-cp ../ParrotMod_Grouper_Stable.zip ~/Google\ Drive/Website\ Downloads/Android/ParrotMod_Grouper_Stable.zip
+mv ~/Google\ Drive/Website\ Downloads/Android/ParrotMod_Grouper_Stable.zip ParrotMod_Grouper_Previous.zip 
+cp ParrotMod_Grouper_Stable.zip ~/Google\ Drive/Website\ Downloads/Android/ParrotMod_Grouper_Stable.zip
