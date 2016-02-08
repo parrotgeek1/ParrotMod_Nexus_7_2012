b3fb042407eff3b73488398daeaeaf63483d3702
Ethan Nelson-Moore <parrotgeek1@gmail.com>
2016-02-08 11:40:54 -0600

    delete the rom specific files

--- edit-prop.sh
+++ edit-prop.sh
@@ -1,6 +1,4 @@
 #!/sbin/sh
 busybox grep -F -v -q 'persist.debug.wfd.enable=1' /system/build.prop && echo 'persist.debug.wfd.enable=1' >> /system/build.prop
-busybox grep -F -v -q 'ro.config.wallpaper=' /system/build.prop && echo 'ro.config.wallpaper=/system/media/wallpaper.jpg' >> /system/build.prop
 busybox grep -F -v -q 'ro.setupwizard.wifi_required=' /system/build.prop && echo 'ro.setupwizard.wifi_required=false' >> /system/build.prop
-busybox sed -i 's/id=LMY47V.*$/id=LMY47V_ParrotMod_2016-02-05_20:14:58/g' /system/build.prop
 busybox grep -F -v -q 'persist.sys.wfd.noimr=1' /system/build.prop && echo 'persist.sys.wfd.noimr=1' >> /system/build.prop
