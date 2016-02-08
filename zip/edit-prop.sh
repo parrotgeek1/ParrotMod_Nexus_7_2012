6bea1b78696538434384fef4cc2fe16b8774f2fe
Ethan Nelson-Moore <parrotgeek1@gmail.com>
2016-02-08 17:41:20 -0600

    universal miracast patch even 6.0

--- zip/edit-prop.sh
+++
@@ -1,4 +0,0 @@
-#!/sbin/sh
-busybox grep -F -v -q 'persist.debug.wfd.enable=1' /system/build.prop && echo 'persist.debug.wfd.enable=1' >> /system/build.prop
-busybox grep -F -v -q 'persist.sys.wfd.nohdcp=1' /system/build.prop && echo 'persist.sys.wfd.nohdcp=1' >> /system/build.prop
-busybox grep -F -v -q 'persist.sys.wfd.noimr=1' /system/build.prop && echo 'persist.sys.wfd.noimr=1' >> /system/build.prop
