a1d3e4decd56c048fb20a604a4b33ce4d06c70b9
Ethan <parrotgeek1@gmail.com>
2016-02-08 12:58:43 -0600

    remove hdcp,the libstagefright doesn't support it

--- zip/edit-prop.sh
+++ zip/edit-prop.sh
@@ -1,4 +1,4 @@
 #!/sbin/sh
 busybox grep -F -v -q 'persist.debug.wfd.enable=1' /system/build.prop && echo 'persist.debug.wfd.enable=1' >> /system/build.prop
-busybox grep -F -v -q 'ro.setupwizard.wifi_required=' /system/build.prop && echo 'ro.setupwizard.wifi_required=false' >> /system/build.prop
+busybox grep -F -v -q 'persist.sys.wfd.nohdcp=1' /system/build.prop && echo 'persist.sys.wfd.nohdcp=1' >> /system/build.prop
 busybox grep -F -v -q 'persist.sys.wfd.noimr=1' /system/build.prop && echo 'persist.sys.wfd.noimr=1' >> /system/build.prop
