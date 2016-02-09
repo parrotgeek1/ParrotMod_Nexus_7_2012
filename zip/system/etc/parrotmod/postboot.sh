e98ddb07809e2b071d38f474347380d33598e6ee
Ethan <parrotgeek1@gmail.com>
2016-02-09 11:43:05 -0600

    restructure (2)

--- zip/system/etc/parrotmod/postboot.sh
+++ zip/system/etc/parrotmod/postboot.sh
@@ -4,6 +4,9 @@ while [ "$($bb pidof bootanimation)" = "" ]; do sleep 1; done
 $bb renice 15 $($bb pidof bootanimation)
 while [ "$(getprop sys.boot_completed)" = "" ]; do sleep 1; done
 sleep 5
+
+# cfq scheduler tuning (noop is better at boot)
+
 cd /sys/block/mmcblk0/queue
 echo cfq > scheduler
 echo 0 > iosched/slice_idle
@@ -11,20 +14,26 @@ echo 0 > iosched/group_idle
 echo 200 > iosched/target_latency # ms
 echo "1" > iosched/low_latency
 echo "1" > iosched/back_seek_penalty # no penalty
-echo "1000000000" > iosched/back_seek_max
+echo "1000000000" > iosched/back_seek_max # i.e. the whole disk
+
+# ram tuning
 # https://01.org/android-ia/user-guides/android-memory-tuning-android-5.0-and-5.1
-# ksm after boot better
+# ksm after boot better because it slows down odexing
+
 cd /sys/kernel/mm/ksm 
 echo 100 > pages_to_scan
 echo 500 > sleep_millisecs
 echo 1 > run
+
+# i/o optimization for specific processes
+
 # for (mostly) fixing audio stutter when multitasking
 $bb renice -15 $($bb pidof mediaserver) #disk io important
 $bb renice -15 $($bb pidof hd-audio0) #avoid underruns
-#cfq adjusts disk io based on nice value
-$bb renice -10 $($bb pidof sdcard)
+$bb renice -10 $($bb pidof sdcard) #cfq adjusts disk io based on nice value
 $bb renice -15 $($bb pidof lmkd) # stop hard freezes from low memory killer being CPU starved
 echo 1 > /sys/devices/system/cpu/cpufreq/interactive/io_is_busy
+
 if [ "$(cat /data/lastpmver.txt)" != "1" ]; then
 	settings put global sys_storage_full_threshold_bytes 8388608
 	settings put global sys_storage_threshold_percentage 2
@@ -32,5 +41,5 @@ if [ "$(cat /data/lastpmver.txt)" != "1" ]; then
 	$bb fstrim -v /data
 	$bb fstrim -v /cache
 	echo 1 > /data/lastpmver.txt
-	setprop ctl.restart zygote
+	setprop ctl.restart zygote # soft reboot
 fi
