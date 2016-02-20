f2a83f7739b97618af9cdd346015a3d8f7a8fc6e
Ethan <parrotgeek1@gmail.com>
2016-02-20 14:12:33 -0600

    Update miracast_patch.sh

--- zip/miracast_patch.sh
+++ zip/miracast_patch.sh
@@ -4,4 +4,3 @@ rm -f /system/etc/parrotmod/libstagefright_wfd.so.orig
 cp /system/lib/libstagefright_wfd.so /system/etc/parrotmod/libstagefright_wfd.so.orig
 # sometimes the most compatible way is the worst way
 LC_ALL=C busybox sed -i 's@intra-refresh-mode@intra-refresh-NOPE@g' /system/lib/libstagefright_wfd.so
-LC_ALL=C busybox sed -i 's@wfd_content_protection@wfd_MISSING_protection@g' /system/lib/libstagefright_wfd.so
