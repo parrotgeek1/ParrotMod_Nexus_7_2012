4bfd0fee1869e3a0b4ecb92649644bb212603253
Ethan Nelson-Moore <parrotgeek1@gmail.com>
2016-03-21 11:51:50 -0500

    more fixes

--- zip/system/etc/parrotmod/postboot.sh
+++ zip/system/etc/parrotmod/postboot.sh
@@ -1,9 +1,11 @@
 #!/system/bin/sh
 bb=/system/etc/parrotmod/busybox
 
-while [ "$(getprop sys.boot_completed)" = "" ]; do sleep 1; done
+while [ "$($bb pidof com.android.systemui)" = "" ]; do sleep 1; done
 
-if [ "$(cat /data/lastpmver_univ.txt)" != "2.0rc4.1" ]; then
+[ -e "/system/etc/parrotmodstock/postboot.sh" ] && /system/bin/sh "/system/etc/parrotmodstock/postboot.sh"
+
+if [ "$(settings get global parrotmod_univ_last_version)" != "2.0rc5" ]; then
 
   settings put global sys_storage_full_threshold_bytes 8388608
   settings put global sys_storage_threshold_percentage 2
@@ -11,16 +13,15 @@ if [ "$(cat /data/lastpmver_univ.txt)" != "2.0rc4.1" ]; then
   settings put global fstrim_mandatory_interval 259200000 # 3 days, same as stock
   settings put global tether_dun_required 0
   
-  echo "2.0rc4.1" > /data/lastpmver_univ.txt
+  settings put global parrotmod_univ_last_version "2.0rc5"
+  $bb rm -f /data/lastpmver.txt /data/lastpmver_univ.txt
   
   am start -a android.intent.action.REBOOT # cleaner reboot
   
 fi
 
-$bb chmod -R 0775 /sys/module/lowmemorykiller/parameters
 echo "0,1,2,4,7,15" > /sys/module/lowmemorykiller/parameters/adj  # https://android.googlesource.com/platform/frameworks/base/+/master/services/core/java/com/android/server/am/ProcessList.java#50
 echo "8192,10240,12288,14336,16384,20480" > /sys/module/lowmemorykiller/parameters/minfree # the same as Moto G 5.1, and AOSP 4.x
-$bb chmod -R 0555 /sys/module/lowmemorykiller/parameters # so android can't edit it
 
 #block ota
 pm disable 'com.google.android.gms/.update.SystemUpdateActivity'
