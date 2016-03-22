cee054867bb004e9f4f3b7f7854dff92a608eed8
Ethan Nelson-Moore <parrotgeek1@gmail.com>
2016-03-21 20:48:47 -0500

    kek

--- mkmod.sh
+++ mkmod.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")/zip"
 branch=$(git rev-parse --abbrev-ref HEAD)
-rm -rf ../ParrotMod_Grouper_{$branch}.zip
+rm -rf ../ParrotMod_Grouper_${branch}.zip
 find . -name '.DS_Store' -delete
-zip -9 -r -q ../ParrotMod_Grouper_{$branch}.zip * 
-du -h ../ParrotMod_Grouper_{$branch}.zip
+zip -9 -r -q ../ParrotMod_Grouper_${branch}.zip * 
+du -h ../ParrotMod_Grouper_${branch}.zip
--- upmod.sh
+++ upmod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 branch=$(git rev-parse --abbrev-ref HEAD)
-rm -f ~/Google\ Drive/Website\ Downloads/Android/ParrotMod_Grouper_Stable_{$branch}.zip
-cp ParrotMod_Grouper_Stable_{$branch}.zip ~/Google\ Drive/Website\ Downloads/Android/ParrotMod_Grouper_Stable_{$branch}.zip
+rm -f ~/Google\ Drive/Website\ Downloads/Android/ParrotMod_Grouper_Stable_${branch}.zip
+cp ParrotMod_Grouper_Stable_${branch}.zip ~/Google\ Drive/Website\ Downloads/Android/ParrotMod_Grouper_Stable_${branch}.zip
