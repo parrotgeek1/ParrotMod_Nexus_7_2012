e1aa030d9821994ed45485c4568c2c5da7c82d9e
Ethan Nelson-Moore <parrotgeek1@gmail.com>
2016-03-22 17:24:05 -0500

    ign bb

--- mkmod.sh
+++ mkmod.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-cd "$(dirname "$0")/zip"
 branch=$(git rev-parse --abbrev-ref HEAD)
+cd "$(dirname "$0")/zip"
+cd system/etc/parrotmod
+rm -f busybox
+curl -# -L -o busybox https://busybox.net/downloads/binaries/busybox-armv5l 
+cd ../../../
 rm -rf ../ParrotMod_Grouper_${branch}.zip
 find . -name '.DS_Store' -delete
 zip -9 -r -q ../ParrotMod_Grouper_${branch}.zip * 
