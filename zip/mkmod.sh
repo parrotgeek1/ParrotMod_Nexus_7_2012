bb2367dcc43722322177c1431c2068593038e092
Ethan Nelson-Moore <parrotgeek1@gmail.com>
2016-03-22 17:38:50 -0500

    fix branch name auto

--- zip/mkmod.sh
+++ zip/mkmod.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 cd "$(dirname "$0")"
-rm -rf ../ParrotMod_Grouper_ChargingNoiseFix.zip
+branch=$(git rev-parse --abbrev-ref HEAD)
+rm -rf ../ParrotMod_Grouper_${branch}.zip
 find . -name '.DS_Store' -delete
-zip -9 -r ../ParrotMod_Grouper_ChargingNoiseFix.zip * -x "mkmod.sh" -x "upmod.sh"
+zip -9 -r ../ParrotMod_Grouper_${branch}.zip * -x "ParrotMod_Grouper_*.zip" -x "Readme.txt" -x "To Do.txt" -x "mkmod.sh" -x "upmod.sh"
