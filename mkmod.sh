ce693cfcecfacde7416dde51047356146f378420
Ethan Nelson-Moore <parrotgeek1@gmail.com>
2016-03-22 17:30:12 -0500

    dont leave bb

--- mkmod.sh
+++ mkmod.sh
@@ -9,3 +9,5 @@ rm -rf ../ParrotMod_Grouper_${branch}.zip
 find . -name '.DS_Store' -delete
 zip -9 -r -q ../ParrotMod_Grouper_${branch}.zip * 
 du -h ../ParrotMod_Grouper_${branch}.zip
+cd system/etc/parrotmod
+rm -f busybox
