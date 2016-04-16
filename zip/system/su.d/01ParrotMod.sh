ff34ba17bc2539df808ce1ff617b588b70411029
Ethan Nelson-Moore <parrotgeek1@gmail.com>
2016-04-16 13:29:52 -0500

    2doetc

--- zip/system/su.d/01ParrotMod.sh
+++ zip/system/su.d/01ParrotMod.sh
@@ -111,7 +111,6 @@ for m in /data /cache /system; do
 	mount | $bb grep "$m" | $bb grep -q ext4 && mount -o remount,noauto_da_alloc,delalloc,journal_async_commit,journal_ioprio=7,barrier=0,commit=15,noatime,nodiratime,inode_readahead_blks=8,dioread_nolock,max_batch_time=15000,nomblk_io_submit "$m" "$m"
 	mount | $bb grep "$m" | $bb grep -q f2fs && mount -o remount,nobarrier,flush_merge,inline_xattr,inline_data,inline_dentry "$m" "$m"
 done
-mount | $bb grep '/system' | $bb grep -q ext4 && mount -o remount,inode_readahead_blks=16 /system /system
 
 for f in /sys/fs/ext4/*; do
 	$bb test "$f" = "/sys/fs/ext4/features" && continue
