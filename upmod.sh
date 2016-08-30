d8d662963087cfdd73ddaccec900c3c0523630e5
Ethan Nelson-Moore <ethan@Ethans-MacBook-Pro.local>
2016-08-30 18:02:34 -0500

    fix upload

--- upmod.sh
+++ upmod.sh
@@ -3,5 +3,5 @@ cd "$(dirname "$0")"
 branch=$(git rev-parse --abbrev-ref HEAD)
 branchend=$(echo $branch|cut -d _ -f 2)
 [ "$branchend" != "Test" ] && rm -rf "ParrotMod/${branch}_Test"
-rm -rf ~/Google\ Drive/Website\ Downloads/Android/ParrotMod
-cp -r ParrotMod ~/Google\ Drive/Website\ Downloads/Android/
+rm -rf /Users/ethan/Desktop/Website/web/downloads/Android/ParrotMod
+cp -r ParrotMod /Users/ethan/Desktop/Website/web/downloads/Android/
