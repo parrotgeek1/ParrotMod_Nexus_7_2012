ae5931d21d6070d300dea906a5aeb4cc091b6dd5
parrotgeek1 <parrotgeek1@gmail.com>
2016-10-14 19:35:08 -0700

    rm upmod

--- upmod.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-branch=$(git rev-parse --abbrev-ref HEAD)
-branchend=$(echo $branch|cut -d _ -f 2)
-[ "$branchend" != "Test" ] && rm -rf "ParrotMod/${branch}_Test"
-rm -rf /Users/ethan/Desktop/Website/web/downloads/Android/ParrotMod
-cp -r ParrotMod /Users/ethan/Desktop/Website/web/downloads/Android/
